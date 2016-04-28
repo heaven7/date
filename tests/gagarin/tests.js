52d557dc512c04a30e098e4067b8616d5a8f9c49
Christian Grlica <christian@heavenseven.net>
2016-04-28 19:01:14 +0200

    refractoring unix2date

--- tests/gagarin/tests.js
+++ tests/gagarin/tests.js
@@ -4,13 +4,13 @@ describe('date package suite', function () {
 
 
     it("should output formatted date", function () {
-        var date = new Date('01.01.2016')
+        var date = new Date('2016-01-01T00:00:00+01:00')
         var value = client.execute(function (val1) {
             moment.locale('en')
             return mDate.formatted(val1)
         }, [date])
 
-        expect(value).to.equal("01.01.2016 12:00")
+        expect(value).to.equal("01.01.2016")
     })
 
     it("should output timeAgo date", function () {
@@ -36,7 +36,7 @@ describe('date package suite', function () {
     it("should transform a unix timestamp into a formatted date", function () {
         var date = 1461751200
         var value = client.execute(function (val1) {
-            return mDate.unix(val1, "DD.MM.YYYY")
+            return mDate.unix2Date(val1, "DD.MM.YYYY")
         }, [date])
 
         expect(value).to.equal("27.04.2016")
